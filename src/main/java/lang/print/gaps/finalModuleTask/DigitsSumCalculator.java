b2313e0a78e0333c9fe7340160e88b10bf49e786
Khamroev <khamroevjs@gmail.com>
2023-06-18 11:58:27 +0300

    task done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int result = 0;
+        while (number != 0) {
+            int lastDigit = number % 10;
+            number = number / 10;
+            result += lastDigit;
+        }
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int result = 0;
+        while (number != 0) {
+            int lastDigit = number % 10;
+            number = number / 10;
+            result = result * 10 + lastDigit;
+        }
+        System.out.println(result);
     }
 }
